1516e9ced3d2c72af2742476ebaa22a503c1b2a8
kotejg <guinho.psp1@gmail.com>
2025-01-07 15:32:36 -0300

    primeiro commit

---
+++ src/test/java/br/com/retrogames/RetroGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.retrogames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetroGamesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
